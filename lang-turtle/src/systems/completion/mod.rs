cd86952aaba6e5bc5fa6ed8d58ec3986e1027935
ajuvercr <arthur.vercruysse@outlook.com>
2024-11-14 17:31:17 +0100

    add lov prefix completion

--- lang-turtle/src/systems/completion/mod.rs
+++ lang-turtle/src/systems/completion/mod.rs
@@ -1,14 +1,9 @@
 mod prefix;
 mod subject;
 
-use lsp_core::{
-    components::{CompletionRequest, RopeC, Tokens},
-    utils::position_to_offset,
-};
+pub use prefix::turtle_lov_prefix_completion;
 pub use prefix::turtle_prefix_completion;
 pub use subject::subject_completion;
 
 #[cfg(test)]
 mod tests;
-
-use bevy_ecs::prelude::*;
--- lang-turtle/src/systems/completion/prefix.rs
+++ lang-turtle/src/systems/completion/prefix.rs
@@ -1,9 +1,71 @@
+use std::collections::HashSet;
+
 use bevy_ecs::prelude::*;
 
-use lsp_core::{components::*, lang::SimpleCompletion};
+use lsp_core::{components::*, lang::SimpleCompletion, token::Token, utils::offset_to_position};
 use lsp_types::CompletionItemKind;
 
 use crate::TurtleLang;
+
+pub fn turtle_lov_prefix_completion(
+    mut query: Query<(
+        &TokenComponent,
+        Option<&Element<TurtleLang>>,
+        &mut CompletionRequest,
+    )>,
+) {
+    for (word, turtle, mut req) in &mut query {
+        match word.token.value() {
+            Token::Invalid(_) => {}
+            _ => continue,
+        }
+
+        let mut start = Position::new(0, 0);
+        let mut defined = HashSet::new();
+        if let Some(t) = turtle {
+            if t.base.is_some() {
+                start = Position::new(1, 0);
+            }
+
+            for pref in &t.prefixes {
+                defined.insert(pref.prefix.value().as_str());
+            }
+        }
+
+        use lsp_types::{Position, Range};
+        req.extend(
+            lov::LOCAL_PREFIXES
+                .iter()
+                .filter(|x| x.name.starts_with(&word.text))
+                .filter(|x| !defined.contains(&x.name))
+                .flat_map(|lov| {
+                    let new_text = format!("{}:", lov.name);
+                    if new_text != word.text {
+                        let edits = vec![
+                            lsp_types::TextEdit {
+                                new_text,
+                                range: word.range.clone(),
+                            },
+                            lsp_types::TextEdit {
+                                range: Range::new(start.clone(), start),
+                                new_text: format!("@prefix {}: <{}>.\n", lov.name, lov.location),
+                            },
+                        ];
+                        Some(SimpleCompletion {
+                            kind: CompletionItemKind::MODULE,
+                            label: format!("{}", lov.name),
+                            documentation: lov.title.to_string().into(),
+                            sort_text: None,
+                            filter_text: None,
+                            edits,
+                        })
+                    } else {
+                        None
+                    }
+                }),
+        );
+    }
+}
 
 pub fn turtle_prefix_completion(
     mut query: Query<(
