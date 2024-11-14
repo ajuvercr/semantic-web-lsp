5318d0622fa80a33183180a93995c4db098893b7
ajuvercr <arthur.vercruysse@outlook.com>
2024-11-14 17:09:12 +0100

    add lov workpackage

---
+++ lov/README.md
@@ -0,0 +1,18 @@
+
+# LOV indexes
+
+
+List of all LOV defined ontologies.
+
+Excluding:
+
+- fiesta-priv
+- open311
+- opm
+- tfo
+- uiots
+- xsd
+
+### Update
+
+Update with `node script.js`.
---
+++ lov/src/lib.rs
@@ -0,0 +1,8 @@
+pub struct LocalPrefix {
+    pub location: &'static str,
+    pub content: &'static str,
+    pub name: &'static str,
+    pub title: &'static str,
+}
+
+pub const LOCAL_PREFIXES: &'static [LocalPrefix] = &[];
