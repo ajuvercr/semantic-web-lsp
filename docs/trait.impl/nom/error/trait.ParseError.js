320f8755f56d7e6f593d1978e8c254262433853f
ajuvercr <ajuvercr@users.noreply.github.com>
2025-06-30 08:59:26 +0000

    deploy: fbad08c82c426670e75f8f6f3127ca83779c237a

--- docs/trait.impl/nom/error/trait.ParseError.js
+++ docs/trait.impl/nom/error/trait.ParseError.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["nom",[]],["shex_compact",[["impl <a class=\"trait\" href=\"nom/error/trait.ParseError.html\" title=\"trait nom::error::ParseError\">ParseError</a>&lt;<a class=\"struct\" href=\"nom_locate/struct.LocatedSpan.html\" title=\"struct nom_locate::LocatedSpan\">LocatedSpan</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.87.0/std/primitive.str.html\">str</a>&gt;&gt; for <a class=\"struct\" href=\"shex_compact/struct.LocatedParseError.html\" title=\"struct shex_compact::LocatedParseError\">LocatedParseError</a>"]]]]);
+    var implementors = Object.fromEntries([["nom",[]],["shex_compact",[["impl <a class=\"trait\" href=\"nom/error/trait.ParseError.html\" title=\"trait nom::error::ParseError\">ParseError</a>&lt;<a class=\"struct\" href=\"nom_locate/struct.LocatedSpan.html\" title=\"struct nom_locate::LocatedSpan\">LocatedSpan</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.88.0/std/primitive.str.html\">str</a>&gt;&gt; for <a class=\"struct\" href=\"shex_compact/struct.LocatedParseError.html\" title=\"struct shex_compact::LocatedParseError\">LocatedParseError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
